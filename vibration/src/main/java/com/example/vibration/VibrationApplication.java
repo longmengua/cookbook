f0061a3a63512fad8f14c5edcbdcd1c17c6aac19
黃鴻文 <longmengua@gmail.com>
2020-04-08 14:25:22 +0800

    insert new project

---
+++ vibration/src/main/java/com/example/vibration/VibrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibrationApplication.class, args);
+	}
+
+}
