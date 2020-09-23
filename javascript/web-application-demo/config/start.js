0c76d2c2bff78665b2c8687f520286040a8367a5
Waltor.Huang <Waltor.Huang@yxunistar.com>
2020-09-23 17:10:25 +0800

    typescript config

--- javascript/web-application-demo/config/start.js
+++ javascript/web-application-demo/config/start.js
@@ -1,134 +1,82 @@
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require("path")
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 // const env = require("./env")
-const root = path.resolve("")
+const root = path.resolve("");
 
 const devServer = {
-    contentBase: path.join("public"),
-    compress: true,
-    port: 1234,
-    hot: true,
-    index: 'index.html',
-}
+	contentBase: path.join("public"),
+	compress: true,
+	port: 1234,
+	hot: true,
+	index: 'index.html',
+};
 
 const resolve = {
-    extensions: [".js"],
-    modules: ["node_modules"],
-}
+	extensions: [ '.tsx', '.ts', '.js' ],
+	modules: ["node_modules"],
+};
 
 const entry = {
-    index: "./src/index.js",
-}
+	index: "./src/index.js",
+};
 
 const output = {
-    path: path.resolve("static"),
-    filename: "[hash:8].js",
-}
+	path: path.resolve("static"),
+	filename: "[hash:8].js",
+};
 
 const plugins = [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-        template: 'index.html'
-    })
-]
+	new webpack.HotModuleReplacementPlugin(),
+	new HtmlWebpackPlugin({
+		template: 'index.html'
+	})
+];
 
 const _module = {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            include: [
-                path.resolve(root,"src")
-            ],
-            use: [{
-                loader: 'babel-loader',
-                // options: {
-                //     presets: ['@babel/preset-react'],
-                // }
-            }]
-        },
-        // {
-        //     test: /\.(js|jsx|ts|tsx)$/,
-        //     include: env.src,
-        //     loader: "ts-loader",
-        //     options: {
-        //         configFile: env.tsConfig,
-        //         transpileOnly: true,
-        //         getCustomTransformers: () => ({
-        //             before: [TSImportPlugin({libraryName: "antd", libraryDirectory: "es", style: true})],
-        //         }),
-        //     },
-        // },
-        // // NOTICE: 待釐清css preprocess
-        // {
-        //     test: /\.(sass)$/,
-        //     use: [
-        //         "style-loader",
-        //         "css-loader",
-        //         'sass-loader',
-        //     ],
-        // },
-        // {
-        //     test: /\.(css|less)$/,
-        //     use: [
-        //         MiniCSSExtractPlugin.loader,
-        //         {
-        //             loader: "css-loader",
-        //             options: {
-        //                 sourceMap: false,
-        //                 importLoaders: 2,
-        //             },
-        //         },
-        //         {
-        //             loader: "postcss-loader",
-        //             options: {
-        //                 sourceMap: false,
-        //                 plugins: () => [autoprefixer],
-        //             },
-        //         },
-        //         {
-        //             loader: "less-loader",
-        //             options: {
-        //                 javascriptEnabled: true,
-        //                 sourceMap: false,
-        //             },
-        //         },
-        //     ],
-        // },
-        // {
-        //     test: /\.(png|jpe?g|gif)$/,
-        //     loader: "url-loader",
-        //     query: {
-        //         limit: 1024,
-        //         name: "static/img/[name].[hash:8].[ext]",
-        //     },
-        // },
-        // {
-        //     test: /\.(woff|woff2|eot|ttf|otf)$/,
-        //     loader: "file-loader",
-        //     options: {
-        //         name: "static/font/[name].[hash:8].[ext]",
-        //     },
-        // },
-        // {
-        //     test: /\.ico$/,
-        //     loader: "file-loader",
-        //     options: {
-        //         name: "static/icon/[name].[hash:8].[ext]",
-        //     },
-        // },
-    ],
-}
+	rules: [
+		/**
+   * @Note babel parsing jsx
+   * */
+		// {
+		// 	test: /\.jsx?$/,
+		// 	exclude: /node_modules/,
+		// 	include: [
+		// 		path.resolve(root,"src")
+		// 	],
+		// 	use: [{
+		// 		loader: 'babel-loader',
+		// 		/**
+       // * @Note this configuration is react
+       // * */
+		// 		// options: {
+		// 		//     presets: ['@babel/preset-react'],
+		// 		// }
+		// 	}]
+		// },
+		/**
+   * @Note tpyescript
+   * @Steps
+   *      1. npm install --save-dev typescript ts-loader
+   *      2. create a tsconfig.json
+   *      3. insert loader in webpack
+   * */
+		{
+			test: /\.tsx?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		},
+	],
+};
 
 const config = {
-    mode: 'none',
-    module: _module,
-    entry,
-    output,
-    devServer,
-    resolve,
-    plugins,
-}
+	mode: 'none',
+	module: _module,
+	entry,
+	output,
+	devServer,
+	resolve,
+	plugins,
+};
 
-module.exports = config
+module.exports = config;
