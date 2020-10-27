71a4ad6e75ec8ef11a330ffeeb7326fdcc43064c
Waltor.Huang <Waltor.Huang@yxunistar.com>
2020-10-27 10:40:34 +0800

    fixed conflict

---
+++ javascript/web-application-demo/src/Maintenance/index.tsx
@@ -0,0 +1,33 @@
+import "./index.sass";
+import "./index_c.css";
+import * as React from "react";
+import {useEffect, useState} from "react";
+import {initMaintenanceState, MaintenanceProps, MaintenanceState} from "./interface";
+import {render} from "react-dom";
+
+const Maintenance: React.FC<MaintenanceProps> = (props: MaintenanceProps) => {
+	const [state, setState] = useState(initMaintenanceState as MaintenanceState);
+	useEffect(()=>{
+		/**
+		 * ... here is for componentDidMount and componentDidUpdate will trigger this
+		 * */
+		/**
+		 * the function given to return is for componentWillUnmount()
+		 * */
+		return ()=> {};
+	}, []);
+	return (
+		<div className={"Maintenance"}>
+			test
+		</div>
+	);
+};
+
+export default Maintenance;
+
+render(
+	<React.StrictMode>
+		<Maintenance />
+	</React.StrictMode>,
+	document.getElementById('root_react')
+);
---
+++ javascript/web-application-demo/src/Maintenance/interface.ts
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export type MaintenanceProps = {
+    children?: React.ReactNode,
+};
+
+export type MaintenanceState = {
+
+};
+
+export const initMaintenanceState: MaintenanceState = {
+
+};
