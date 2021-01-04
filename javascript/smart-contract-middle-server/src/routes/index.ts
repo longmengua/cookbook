a30230d2875d2b70392dc630dcebf87033b6e549
waltor <longmengua@gmail.com>
2021-01-04 13:29:08 +0800

    remove ResponseEntity

--- javascript/smart-contract-middle-server/src/routes/index.ts
+++ javascript/smart-contract-middle-server/src/routes/index.ts
@@ -1,15 +1,5 @@
 import { Router } from 'express';
 import UserRouter from './Users/Users';
-//Status code enum
-enum MsgCode {
-    successful = "successful",
-    error = "error",
-}
-//Response interface
-interface ResponseEntity {
-    msgCode: MsgCode,
-    data: any,
-}
 
 // Init router and path
 const router = Router();
@@ -19,4 +9,3 @@ router.use('/users', UserRouter);
 
 // Export the base-router
 export default router;
-export { ResponseEntity, MsgCode };
